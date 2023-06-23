a65093cec8efffe5b16fd7b00544f362216a2735
Chronohole <35706287+ChronoHole@users.noreply.github.com>
2023-06-23 19:26:26 +0500

    update alert

--- assets/ios.transition-legacy-da50f76c.js
+++ assets/ios.transition-legacy-da50f76c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-08fe9cd8.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-40136555.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
